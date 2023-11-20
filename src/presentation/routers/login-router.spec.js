fcd0fbbd76a2fdc037ebe7d0bdc3e7ec8585e877
Iago Carvalho <iagosilva.ti@gmail.com>
2023-11-20 17:40:03 -0300

    refactoring move generic errors to utils layer

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,6 @@
 const LoginRouter = require('./login-router')
-const { MissingParamError, InvalidParamError, ServerError, UnauthorizedError } = require('../errors/index')
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { ServerError, UnauthorizedError } = require('../errors/index')
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
