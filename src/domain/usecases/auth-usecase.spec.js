626a71636818a1f7ef4d71e8404d1edc58ff9ecc
Iago Carvalho <iagosilva.ti@gmail.com>
2023-11-25 12:06:39 -0300

    ensure AuthUseCase returns accessToken if correct credentials are provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -111,4 +111,11 @@ describe('Auth Case', () => {
     await sut.Auth('valid_email@mail.com', 'valid_password')
     expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
   })
+
+  test('Should return an accessToke if correct credentials are provided', async () => {
+    const { sut, tokenGeneratorSpy } = makeSut()
+    const accessToken = await sut.Auth('valid_email@mail.com', 'valid_password')
+    expect(accessToken).toBe(tokenGeneratorSpy.accessToken)
+    expect(accessToken).toBeTruthy()
+  })
 })
