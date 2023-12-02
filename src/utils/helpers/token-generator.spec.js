6fd067a8d576846666664db03bf7dcf492069134
Iago Carvalho <iagosilva.ti@gmail.com>
2023-12-02 15:45:53 -0300

    ensure TokenGenerator throws if no id is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -6,8 +6,11 @@ class TokenGenerator {
   }
 
   async generate (id) {
-    if (!id) {
+    if (!this.secret) {
       throw new MissingParamError('secret')
+    }
+    if (!id) {
+      throw new MissingParamError('id')
     }
     return jwt.sign(id, this.secret)
   }
@@ -42,5 +45,11 @@ describe('Token Generator', () => {
     const sut = new TokenGenerator()
     const promise = sut.generate('any_id')
     expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw if no id is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
   })
 })
