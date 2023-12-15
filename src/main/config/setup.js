eabb5ec9c484309d2fc9b317609eb2b77e8b5660
Iago Carvalho <iagosilva.ti@gmail.com>
2023-12-15 09:18:54 -0300

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,9 @@
 module.exports = app => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    res.set('access-control-alow-origin', '*')
+    res.set('access-control-alow-methods', '*')
+    res.set('access-control-alow-headers', '*')
+    next()
+  })
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -3,10 +3,20 @@ const app = require('./app')
 
 describe('App', () => {
   test('Should disable x-powered-by header', async () => {
-    app.get('/test_x_powered-by header', (req, res) => {
+    app.get('/test_x_powered-by', (req, res) => {
       res.send('')
     })
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    app.get('/test-cors', (req, res) => {
+      res.send('')
+    })
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-alow-origin']).toBe('*')
+    expect(res.headers['access-control-alow-methods']).toBe('*')
+    expect(res.headers['access-control-alow-headers']).toBe('*')
   })
 })
