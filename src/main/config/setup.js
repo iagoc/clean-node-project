76e29da35b4d0e45f8180e3f64c5c7048c2033bb
Iago Carvalho <iagosilva.ti@gmail.com>
2023-12-16 14:51:07 -0300

    add routes file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,10 +1,12 @@
 const cors = require('../middlewares/cors')
 const jsonParser = require('../middlewares/json-parser')
 const contentType = require('../middlewares/content-type')
+const setupRoutes = require('./routes')
 
 module.exports = app => {
   app.disable('x-powered-by')
   app.use(cors)
   app.use(jsonParser)
   app.use(contentType)
+  app.use(setupRoutes)
 }
---
+++ src/main/routes/login-routes.js
@@ -0,0 +1,5 @@
+module.exports = router => {
+  router.post('/api/login', () => {
+
+  })
+}
